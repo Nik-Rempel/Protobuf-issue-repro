cc360070ce349067c08930653234abb224b0eee8
Nikita Rempel <nikita.rempel@trayport.com>
2022-01-19 16:45:54 +0000

    Reproduction of the issue we are seeing.

---
+++ Client app/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace Client_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stream = new MemoryStream();
+            Library.ProtoBufTest.Serialize(stream);
+        }
+    }
+}
---
+++ Library/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using ProtoBuf;
+
+namespace Library
+{
+    public static class ProtoBufTest
+    {
+        public static void Serialize(Stream stream)
+        {
+            ProtoBuf.Serializer.Serialize(stream, new EnumWrapper
+            {
+                TestTuple = (EnumKey.Key0, EnumValue.Test2)
+            });
+        }
+    }
+
+    [ProtoContract]
+    public class EnumWrapper
+    {
+        [ProtoMember(1)] public (EnumKey, EnumValue) TestTuple;
+
+    }
+
+    [ProtoContract(EnumPassthru = true)]
+    public enum EnumValue
+    {
+        Test0,
+        Test1,
+        Test2
+    }
+
+    [ProtoContract(EnumPassthru = true)]
+    public enum EnumKey
+    {
+        Key0,
+        Key1,
+        Key2
+    }
+}
